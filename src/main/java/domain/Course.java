1628c69054d61d6d650b9b5f4158fc063e7a375e
Adam <adamfgcross@gmail.com>
2024-09-24 11:43:55 -0400

    create Course entity

---
+++ src/main/java/domain/Course.java
@@ -0,0 +1,45 @@
+package domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private Integer hours;
+	
+	public Course() {}
+	
+	public Course(Integer hours, String name) {
+		this.hours = hours;
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+
+	public Long getId() {
+		return id;
+	}
+}
