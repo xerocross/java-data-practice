30afb87d4b259d6e81852ad6d59a7a98c362753e
Adam <adamfgcross@gmail.com>
2024-09-24 11:33:51 -0400

    initial commit

---
+++ src/test/java/com/adamfgcross/java_data_practice/JavaDataPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.java_data_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDataPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
