6c869025f68e5618846e38ee9378e9f3cfcd3095
Adam <adamfgcross@gmail.com>
2024-09-24 11:45:23 -0400

    create User entity

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,27 @@
+package domain;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Long getId() {
+		return id;
+	}
+}
